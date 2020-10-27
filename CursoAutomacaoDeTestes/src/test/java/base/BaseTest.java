63b509fa531a17c0ebde77d4226c61e78c719bcc
Verônica Souza <veronicasp54@gmail.com>
2020-10-26 22:38:26 -0300

    Cap.2 WebDriver

---
+++ CursoAutomacaoDeTestes/src/test/java/base/BaseTest.java
@@ -0,0 +1,44 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    public static void main (String args[]){
+        BaseTest test = new BaseTest();
+        test.setUp();
+
+    }
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://the-internet.herokuapp.com/");
+       // System.out.println("Sistema acessado: " + driver.getTitle());
+
+        clickLink();
+
+    }
+
+
+    public void clickLink(){
+        WebElement inputLink = driver.findElement(By.linkText("Shifting Content"));
+        inputLink.click();
+
+        WebElement linkExample1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        linkExample1.click();
+
+        List<WebElement> linksMenu = driver.findElements(By.tagName("li"));
+        System.out.println("Quantidade de Links Menu"+ linksMenu.size());
+
+    }
+
+}
