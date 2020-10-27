44a7ed273f371fcb50397b6f8ec92f35553bf4b7
Verônica Souza <veronicasp54@gmail.com>
2020-10-26 23:56:39 -0300

    Cap.3-4.1 Elements

---
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+public class BaseTests {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+
+        driver.quit();
+    }
+
+    public static void main(String args[]){
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
